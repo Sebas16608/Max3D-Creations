30f1ef29ea7ce7b96ac28eb3aee9f22a45c90a36
Sebas16608 <asebasrr444@gmail.com>
2025-09-10 23:10:27 -0600

    creando los modelos para clientes y facturacion este ultimo esta en proceso

--- backend/clientes/models.py
+++ backend/clientes/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Cliente(models.Model):
-    full_name = models.CharField(max_length=100)
-    email = models.EmailField()
+class Datos_Personales(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=25)
+    telefono_alterno = models.CharField(max_length=25)
+
+class Datos_Entrega(models.Model):
     direccion = models.TextField()
-    
+    codigo_acceso = models.CharField(max_length=200)
+    departamento = models.CharField(max_length=200)
+    municipio = models.CharField(max_length=200)
+
--- backend/facturacion/models.py
+++ backend/facturacion/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
 # Create your models here.
+class Datos_Factura(models.Model):
+    nombre = models.CharField(max_length=255)
+    IDENTIFICACIONES = [
+        ("CUI", "CUI"),
+        ("PASAPORTE", "Pasaporte"),
+        ("NIT", "NIT"),
+    ]
+    identificacion = models.CharField(max_length=200, choices=IDENTIFICACIONES)
+
+    def opcion(self):
+        if self.identificacion == "CUI":
+            return "Numero de CUI"
+        elif self.identificacion == "Pasaporte":
+            return "Numero de Pasaporte"
+        elif self.identificacion == "NIT":
+            return "Numero de NIT"
+        else:
+            return "Por favor llenar el campo"
