8c8269d146daa402243a642219a5d8e0d9c89713
Sebas16608 <asebasrr444@gmail.com>
2025-09-11 23:22:01 -0600

    Registrando los modelos en el admin

--- backend/facturacion/admin.py
+++ backend/facturacion/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Datos_Factura, Items_Factuar
 # Register your models here.
+admin.site.register(Datos_Factura)
+admin.site.register(Items_Factuar)
