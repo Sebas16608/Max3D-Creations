31ba80f072fa3ef2327a213253a9d023a8e76e85
Sebas16608 <asebasrr444@gmail.com>
2025-09-11 23:03:41 -0600

    Registrando los modelos en el admin

--- backend/clientes/admin.py
+++ backend/clientes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Datos_Personales, Datos_Entrega
 # Register your models here.
+admin.site.register(Datos_Personales)
+admin.site.register(Datos_Entrega)
