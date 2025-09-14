7b473a30166629f4db99d99fbc90f7d6f62eaea3
Sebas16608 <asebasrr444@gmail.com>
2025-09-14 17:27:38 -0600

    Creando los modelos de pedidos

--- backend/pedidos/models.py
+++ backend/pedidos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from clientes.models import Datos_Personales, Datos_Entrega
-
+from producto.models import Productos
 class Pedido(models.Model):
-    cliente = models.ForeignKey(Datos_Personales, on_delete=models.CASCADE, related_name="entrega")
-    entrega = models.ForeignKey(Datos_Entrega, on_delete=models.CASCADE, related_name="entrega")
+    cliente = models.ForeignKey(Datos_Personales, on_delete=models.CASCADE, related_name="pedidos")
+    entrega = models.ForeignKey(Datos_Entrega, on_delete=models.CASCADE, related_name="pedidos_entrega")
+    productos = models.ManyToManyField(Productos, through="PedidoProductos")
+    cantidad = models.IntegerField()
     fecha = models.DateField(auto_now_add=True)
