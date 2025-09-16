22102b1af9312190976a73adb019a6cc712297d2
Sebas16608 <asebasrr444@gmail.com>
2025-09-16 11:30:41 -0600

    Definiendo el metodo GET de la API de Items de la Facturacion

--- backend/facturacion/views.py
+++ backend/facturacion/views.py
@@ -17,12 +17,25 @@ class DatosView(APIView):
                 serializer = DatosSerializer(datos)
                 return Response(serializer.data, status=status.HTTP_200_OK)
             except Datos_Factura.DoesNotExist:
-                return Response(status=status.HTTP_404_NOT_FOUND)
+                return Response({"error": "Los datos de la Factura no se encuentran"},status=status.HTTP_404_NOT_FOUND)
         else:
             datos = Datos_Factura.objects.all()
             serializer = DatosSerializer(datos, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         
 """
-
+Items API
 """
+class ItemsView(APIView):
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                items = Items_factura.objects.get(pk=pk)
+                serializer = ItemsSerializer(items)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except Items_factura.DoesNotExist:
+                return Response({"error": "Los items de la factura no se encuentran"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            items = Items_factura.objects.all()
+            serializer = ItemsSerializer(items, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
