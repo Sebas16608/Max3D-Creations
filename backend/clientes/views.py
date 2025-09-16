2f35a37aa4f127fabd597ec2b70838d2a282e2b4
Sebas16608 <asebasrr444@gmail.com>
2025-09-16 12:53:27 -0600

    Definiendo el metodo DELETE de la API de datos

--- backend/clientes/views.py
+++ backend/clientes/views.py
@@ -80,3 +80,12 @@ class DatosView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        try:
+            datos = Datos_Entrega.objects.get(pk=pk)
+        except Datos_Entrega.DoesNotExist:
+            return Response({"error", "Los datos de la entrega no se encuentran"}, status=status.HTTP_404_NOT_FOUND)
+        
+        datos.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
