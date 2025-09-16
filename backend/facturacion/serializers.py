bd0980424cb1565b996224dd148c2380711a4309
Sebas16608 <asebasrr444@gmail.com>
2025-09-16 09:38:12 -0600

    Cambiando el nombre de los serializadores

--- backend/facturacion/serializers.py
+++ backend/facturacion/serializers.py
@@ -1,12 +1,12 @@
 from .models import Datos_Factura, Items_factura
 from rest_framework import serializers
 
-class DatosView(serializers.ModelSerializer):
+class DatosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Datos_Factura
         fields = "__all__"
 
-class ItemsView(serializers.ModelSerializer):
+class ItemsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Items_factura
         fields = "__all__"
