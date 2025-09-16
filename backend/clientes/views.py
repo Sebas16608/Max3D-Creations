5b2a2902bfb55e246abfed6ddac7436867e82316
Sebas16608 <asebasrr444@gmail.com>
2025-09-15 18:56:19 -0600

    Creando metodo GET y POST de la API de clientes

--- backend/clientes/views.py
+++ backend/clientes/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from .models import Datos_Personales, Datos_Entrega
+from .serializers import PersonalSerializers, EntregaSerializers
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 # Create your views here.
+class PersonalView(APIView):
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                datos_personales = Datos_Personales.objects.get(pk=pk)
+                serializer = PersonalSerializers(datos_personales)
+                return Response(serializer.data)
+            except Datos_Personales.DoesNotExist:
+                return Response({"error", "Los datos del cliente no existen"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            datos_personales = Datos_Personales.objects.all()
+            serializer = PersonalSerializers(datos_personales)
+            return Response(serializer.data)
+        
+    def post(self, request):
+        serializer = PersonalSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
