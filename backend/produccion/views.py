12db6212f66d1e0b425f5a0db91bff7fd270ed7d
Sebas16608 <asebasrr444@gmail.com>
2025-09-24 23:11:06 -0600

    Definiendo el metodo GET de la API de produccion

--- backend/produccion/views.py
+++ backend/produccion/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Produccion, ProduccionProducto
+from .serializers import ProduccionSerializer, ProduccionProductoSerializer
+from rest_framework import status
 
-# Create your views here.
+class ProduccionView(APIView):
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                producciones = Produccion.objects.get(pk=pk)
+                serializer = ProduccionSerializer(producciones)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except Produccion.DoesNotExist:
+                return Response({"error": "La produccion no fue encontrada"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            producciones = Produccion.objects.all()
+            serializer = ProduccionSerializer(producciones, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
