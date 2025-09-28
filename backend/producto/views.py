a2fc067ca3af4959ca95aa3fb5a523a1d2c4c7d7
Sebas16608 <asebasrr444@gmail.com>
2025-09-27 22:46:06 -0600

    Definiendo el metodo GET y POST de la API de productos

--- backend/producto/views.py
+++ backend/producto/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Productos
+from .serializers import ProdusctoSerializer
 # Create your views here.
