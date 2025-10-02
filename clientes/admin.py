9b4e0ad839a9aef80ce3ecea82f5063942dacc48
Sebas16608 <asebasrr444@gmail.com>
2025-10-02 00:42:14 -0600

    creando los modelos y registrandolos en el admin, quedarian pendientes las URLs y las APIs

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import DatosPersonales, DatosEntrega, DatosFactura
 # Register your models here.
+admin.site.register(DatosPersonales)
+admin.site.register(DatosEntrega)
+admin.site.register(DatosFactura)
