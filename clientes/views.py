5429ccc723141be5e968f2978bc4777b835509a2
Sebas16608 <asebasrr444@gmail.com>
2025-10-02 19:10:03 -0600

    Definiendo el metodo GET de la API de clientes

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import PersonalSerializer, EntregaSerializer, FacturaSerializer
+from .models import DatosPersonales, DatosEntrega, DatosFactura
 
-# Create your views here.
+
+# Funcion para cuando no existan los datos
+def notexist():
+    return "Los datos no fueron encontrados"
+
+class PersonalView(APIView):
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                personal = DatosPersonales.objects.get(pk=pk)
+                serializer = PersonalSerializer(personal)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except DatosPersonales.DoesNotExist:
+                return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
+        else:
+            personal = DatosPersonales.objects.all()
+            serializer = PersonalSerializer(personal, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
