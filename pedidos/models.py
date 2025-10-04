d0077a82051201ff7379145dc4c5908ca1634194
Sebas16608 <asebasrr444@gmail.com>
2025-10-04 15:07:54 -0600

    Creando los modelos de pedidos pero necesito crear antes los del carrito para hacer un foreingkey probablemente los modelos actuales de pedidos cambien

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Pedido(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField()
+
+class ImagenPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, related_name='imagenes')
+    imagen = models.ImageField(upload_to='pedidos/')
