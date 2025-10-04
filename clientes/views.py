62ddc23d3ffd4943b5151f766f7ad94c65be0101
Sebas16608 <asebasrr444@gmail.com>
2025-10-03 23:02:44 -0600

    El pasado era GET ahora si es POST

--- clientes/views.py
+++ clientes/views.py
@@ -62,5 +62,9 @@ class EntregaView(APIView):
             serializer = EntregaSerializer(entrega, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         
-        
-        
+    def post(self, request):
+        serializer = EntregaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
