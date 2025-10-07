2c380e8daa211dbbd211982017ce4b9e86c1b7c0
Sebas16608 <asebasrr444@gmail.com>
2025-10-06 22:10:22 -0600

    Añadiendo el apartado del carrito en los models de pedidos

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from carrito.models import Carrito
 # Create your models here.
 class Pedido(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE, related_name="pedido")
 
 class ImagenPedido(models.Model):
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, related_name='imagenes')
