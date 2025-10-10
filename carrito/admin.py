a8ae99238e5b10c53878b764698a935f5e413ba4
Sebas16608 <asebasrr444@gmail.com>
2025-10-10 17:59:16 -0600

    Registrando el modelo de carrito item en el admin y creando el serializador del carrito item

--- carrito/admin.py
+++ carrito/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Carrito
+from .models import Carrito, CarritoItem
 # Register your models here.
 admin.site.register(Carrito)    
+admin.site.register(CarritoItem)
--- carrito/serializers.py
+++ carrito/serializers.py
@@ -1,7 +1,12 @@
-from .models import Carrito
+from .models import Carrito, CarritoItem
 from rest_framework import serializers
 
 class CarritoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Carrito
+        fields = "__all__"
+
+class CarritoItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarritoItem
         fields = "__all__"
