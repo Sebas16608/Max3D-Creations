1e7ed36037d6a2b32404ede52dd5ce13eddb64c5
Sebas16608 <asebasrr444@gmail.com>
2025-10-10 17:50:50 -0600

    Creando el modelo de la APP productos

--- producto/models.py
+++ producto/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(50)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    imagen = models.ImageField(upload_to="media-productos/")
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now_add=True)
+    
+    CATEGORIAS = [
+        ("FIGURA", "Figura Decorativa"),
+        ("PIEZA", "Pieza Funcional"),
+        ("PERSONALIZADO", "Diseño Personalizado")
+    ]
+    categorias = models.CharField(max_length=255, choices=CATEGORIAS, default="PIEZA")
+    disponible = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Pedido"
+        verbose_name_plural = "Pedidos"
+
+    def __str__(self):
+        return self.nombre
