efe75be2531c64c23c9f225e3932f26833316784
Sebas16608 <asebasrr444@gmail.com>
2025-10-09 21:55:32 -0600

    Poniendo class meta que no lo puse y tenia un error a la hora de serializar los datos

--- user/serializers.py
+++ user/serializers.py
@@ -2,6 +2,7 @@ from .models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
-    model = User
-    fields = "__all__"
+    class Meta:
+        model = User
+        fields = "__all__"
     
