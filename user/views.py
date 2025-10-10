66e0c227b5eafe28675ffeccc6fdabcb984a198e
Sebas16608 <asebasrr444@gmail.com>
2025-10-09 21:22:11 -0600

    Definiendo el metodo PUT de la API de user

--- user/views.py
+++ user/views.py
@@ -31,3 +31,14 @@ class UserView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    def put(self, request, pk):
+        try:
+            users = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
+        serializer = UserSerializer(users, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
