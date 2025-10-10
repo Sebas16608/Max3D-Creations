1b03f25feceb7b6e52975df8ee2fe9c4f02fbc14
Sebas16608 <asebasrr444@gmail.com>
2025-10-09 18:25:37 -0600

    Definiendo el metodo GET de la API de los usuarios

--- user/views.py
+++ user/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from .serializers import UserSerializer
+from .models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+
+# Funcion por si los datos no existen
+def notexist():
+    return {"error": "Los datos no existen"}
+
+class UserView(APIView):
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                users = User.objects.get(pk=pk)
+                serializer = UserSerializer(users)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except User.DoesNotExist:
+                return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
+        else:
+            users = User.objects.all()
+            serializer = UserSerializer(users, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        
