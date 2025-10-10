0094799635baf5d33b74bc349fc9e253906bd9b1
Sebas16608 <asebasrr444@gmail.com>
2025-10-09 21:13:41 -0600

    Definiendo el metodo POST de la API de user

--- user/views.py
+++ user/views.py
@@ -24,4 +24,10 @@ class UserView(APIView):
             serializer = UserSerializer(users, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         
-        
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
