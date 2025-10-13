ab012f9d468977a8c88c2f6e134c18000f20c69b
Sebas16608 <asebasrr444@gmail.com>
2025-10-13 02:21:31 -0600

    Arreglando lineas de codigo que estaban mal

--- producto/models.py
+++ producto/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Producto(models.Model):
-    nombre = models.CharField(50)
+    nombre = models.CharField(max_length=50)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
@@ -19,8 +19,8 @@ class Producto(models.Model):
     disponible = models.BooleanField(default=True)
 
     class Meta:
-        verbose_name = "Pedido"
-        verbose_name_plural = "Pedidos"
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
 
     def __str__(self):
         return self.nombre
