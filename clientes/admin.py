2d0d41a0aa320c83b303f754ce34a7898f7d650e
Sebas16608 <asebasrr444@gmail.com>
2025-10-14 22:40:33 -0600

    Añadiendo variables de entorno al settings borrando la app clientes ya que no nos servira mas y arreglando las urls del proyecto

--- clientes/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import DatosPersonales, DatosEntrega, DatosFactura
-# Register your models here.
-admin.site.register(DatosPersonales)
-admin.site.register(DatosEntrega)
-admin.site.register(DatosFactura)
