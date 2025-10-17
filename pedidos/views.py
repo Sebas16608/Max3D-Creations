ad4547d5503635d65c537c5e655eff30fc1a0923
Sebas16608 <asebasrr444@gmail.com>
2025-10-16 18:57:09 -0600

    Empezando a crear la API de la app de Pedidos y definiendo el metodo GET de la API

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Pedido
+from .serializers import PedidoSerializer
 # Create your views here.
+
+# Funcion
+def notexits():
+    return {"error": "Los Datos no Fueron encontrados"}
+
+class Pedido(APIView):
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                pedido = Pedido.objects.get(pk=pk)
+                serializer = PedidoSerializer(pedido)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except Pedido.DoesNotExist:
+                return Response(notexits(), status=status.HTTP_404_NOT_FOUND)
